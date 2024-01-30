a31e650be66eddcb79abbd2db16198e578f10ea4
deepak <deepak.kumaran.10@gmail.com>
2024-01-30 23:14:48 +0530

    using basic functions

--- python/arithmetic_operators.py
+++ python/arithmetic_operators.py
@@ -1,7 +1,7 @@
 a = int(input("Enter value of a : "))
 b = int(input("Enter value of b : "))
 c = a+b
-print("The addition of two numbers \n",c)
+print("The addition of two numbers ",c)
 c = a-b
 print("The subtraction of two numbers ",c)
 c = a/b
