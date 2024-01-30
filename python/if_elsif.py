67ef10f3128c2db0ccc453f10aa9f57a75539976
deepak <deepak.kumaran.10@gmail.com>
2024-01-30 23:13:10 +0530

    using basic functions

--- python/if_elsif.py
+++ python/if_elsif.py
@@ -1,5 +1,9 @@
-n = int(input("Enter a number : "))
-if n%2==0:
-    print("it is a even number")
+n = int(input("Enter a number :" ))
+if (n<10):
+    print("It is a single digit number")
+elif (n<100):
+    print("It is a double digit number")
+elif(n<1000):
+    print("It is a three digit number")
 else:
-    print("it is a odd number")
+    print("It is either four or greater than four digit number")
