768bb9ed9367f308484afe900e87ef1e3c0fded6
deepak <deepak.kumaran.10@gmail.com>
2024-01-30 23:05:13 +0530

    using arithmetic operations

--- python/arithmetic_operators.py
+++ python/arithmetic_operators.py
@@ -1,4 +1,11 @@
 a = int(input("Enter value of a : "))
 b = int(input("Enter value of b : "))
 c = a+b
-print("the asnwer is ",c)
+print("The addition of two numbers \n",c)
+c = a-b
+print("The subtraction of two numbers ",c)
+c = a/b
+print("The division of two numbers ",c)
+c = a*b
+print("The multiplication of two numbers ",c)
+
